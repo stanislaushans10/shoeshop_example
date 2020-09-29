f2cc84ed8817faf5515335805cf07faae549c7c4
stanislaushans10 <51685434+stanislaushans10@users.noreply.github.com>
2020-09-29 12:50:24 +0700

    Delete noZero.js

--- script/noZero.js
+++
@@ -1,7 +0,0 @@
-function validateForm() {
-    var x = document.forms["simpananPokok"]["nominal"].value;
-      if (x == "0") {
-        alert("Nominal Tidak Boleh 0");
-        return false;
-      }
-}
